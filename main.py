1f5e3b8b0d9e69c0484a94f6bf71a8b578183a7a
Alan Newton <128156285+newtonalan@users.noreply.github.com>
2023-03-27 10:46:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ metros_quadrados = float(metros_quadrados)
 
 # Coloque o código para resolver o problema nessa parte do programa
 
+lit = metros_quadrados / 3
+
+if 0 < metros_quadrados <= 54:
+  print(f"Para cobeir {metros_quadrados} m² só vai ser necessária 1 lata no valor de R$ 80,00.")
+
+elif metros_quadrados == 0:
+  print("Se você não quer pintar, vá pastar.")
+  
+else: 
+  qtd_de_latas = lit / 18
+  if lit % 18 > 0:
+    qtd_de_latas += 1
+  valor_total = int(qtd_de_latas) * 80
+
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
+
 qtd_de_latas = 0
 valor_total = 0
 
